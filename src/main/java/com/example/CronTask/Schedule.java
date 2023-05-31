32e45a4ad6f3fd3a870da18b2c20b173b4d5dab1
alghaffri <isami.abdullah@gmail.com>
2023-05-31 15:06:32 +0400

    Q29

--- src/main/java/com/example/CronTask/Schedule.java
+++ src/main/java/com/example/CronTask/Schedule.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Schedule {
-    @Scheduled(cron = "* 0 12 3#3 * *")
+    @Scheduled(cron = "* 0 22 * 7-9 0")
     public void Schedule(){
         System.out.println("Hello World");
     }
