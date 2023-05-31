fce8032cb290924cf04187411dab2f5360a0e0da
alghaffri <isami.abdullah@gmail.com>
2023-05-31 11:33:46 +0400

    Add EnableScheduling

--- src/main/java/com/example/CronTask/CronTaskApplication.java
+++ src/main/java/com/example/CronTask/CronTaskApplication.java
@@ -2,8 +2,10 @@ package com.example.CronTask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CronTaskApplication {
 
 	public static void main(String[] args) {
